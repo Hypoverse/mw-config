e564283227a02ebdf9f73a90d5f590262e2dc0ae
Megacane <29050934+Ultracane1997@users.noreply.github.com>
2024-01-28 19:54:58 -0500

    Delete LocalWiki.disasters.php

--- LocalWiki.disasters.php
+++
@@ -1,5 +0,0 @@
-<?php
-$wgSitename = "Hypoverse Disasters";
-$wgMetaNamespace = "Hypoverse_Disasters";
-$wgServer = "https://disasters.hypoverse.org";
-$wgDBname = $hvInstallationPrefix . 'disasters_wiki';
